e69ff8128c62c39af8a8b1a45b4e0f7ed6bf14b4
キャンド <amehikoji@gmail.com>
2023-04-20 23:05:35 -0400

    Clean tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -6,9 +6,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 from Anilist import QueryClient, Scheme
 import logging
+from unittest import TestCase
 
-print(Scheme._construct(Scheme().A.B(k='$3'), Scheme().A.B.C, Scheme().A.B.C(k="$4").D))
+class BaseTest(TestCase):
 
-q = QueryClient(logging.WARNING)
+    def test_scheme(self):
+        _ = Scheme._construct(Scheme().A.B(k='$3'), Scheme().A.B.C, Scheme().A.B.C(k="$4").D)
+    
+    def test_query(self):
+        q = QueryClient(logging.WARNING)
 
-print(q.media_list(username="xxxAnn", per_page=100).entries[0])
+        q.media_list(username="xxxAnn", per_page=100).entries[0]
