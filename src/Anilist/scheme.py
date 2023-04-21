51c595499a38cb25b62a4ef0a29e947d4ef6d7c6
キャンド <amehikoji@gmail.com>
2023-04-20 20:20:26 -0400

    Modify Scheme so that they can have query parameters

--- src/Anilist/scheme.py
+++ src/Anilist/scheme.py
@@ -1,6 +1,7 @@
 class Scheme:
-    def __init__(self, t=None):
+    def __init__(self):
         self._layers = []
+        self._up = {}
 
     def __getattr__(self, k):
         return self[k]
@@ -9,10 +10,16 @@ class Scheme:
         self._layers.append(k)
         return self
     
+    def __call__(self, **kwargs):
+        d = dict(**kwargs)
+        if d != {}:
+            self._up[self._layers[-1]] = f"{self._layers[-1]} ({', '.join([f'{k}: {v}' for k, v in d.items()])})"
+        return self
+    
     @classmethod
-    def _construct(cls, *sch):
-        return construct_query_as_string(construct_query(list(sch)))
-
+    def _construct(cls, *schs):
+        return construct_query_as_string(construct_query(list(schs)), [sch._up for sch in schs])
+    
     
 def construct_query(schs: list[Scheme]):
     temp = {}
@@ -24,21 +31,28 @@ def construct_query(schs: list[Scheme]):
         for i in range(1, len(sch)):
             if not sch[i] in get_nest_list(get_numbers_up_to(i), temp, i, sch):
                 get_nest_list(get_numbers_up_to(i), temp, i, sch)[sch[i]] = {}
+
     
     return temp
 
-def construct_query_as_string(d, ss=""):
+def construct_query_as_string(d, l, ss=""):
     if type(d) != dict:
         return str(d)
     s = ""
     for k, v in d.items():
         if v == {}:
-            s = f"{s}{ss}{k}\n"
+            s = f"{s}{ss}__SPECIAL_KEY__{k}\n"
         else:
-            t = construct_query_as_string(v, f'{ss}  ')
+            t = construct_query_as_string(v, l, f'{ss}  ')
             t = t[0:-1]
-            s = f"{s}{ss}{k} {{\n{t}\n{ss}}}\n"
-    
+            s = f"{s}{ss}__SPECIAL_KEY__{k} {{\n{t}\n{ss}}}\n"
+
+    if ss == "":
+        for dic in l:
+            for k, v in dic.items():
+                s = s.replace(f"__SPECIAL_KEY__{k}", f"{v}")
+        s = s.replace("__SPECIAL_KEY__", "")
+
     return s
 
 
@@ -61,5 +75,5 @@ def get_nest_list(n, l, m, sch):
     # a d v
 class MediaScheme(Scheme):
     def __init__(self):
-        super().__init__(None)
+        super().__init__()
         self._layers.append("media")
--- tests/test_base.py
+++ tests/test_base.py
@@ -4,8 +4,10 @@ srcdir = '../src'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 
-from Anilist import QueryClient
+from Anilist import QueryClient, Scheme
 import logging
+
+print(Scheme._construct(Scheme().A.B(k='$3'), Scheme().A.B.C, Scheme().A.B.C(k="$4").D))
 
 q = QueryClient(logging.WARNING)
 
